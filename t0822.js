ad48ece1d9cc107e6d0241fa30d3544d78e3be67
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-08-22 15:04:40 +0800

    Update t0822.js

--- t0822.js
+++ t0822.js
@@ -8,20 +8,27 @@ function find(num1, num2) {
     let result = [];
     for (let i = 0; i < num1.length; i++) {
         let n = i;
-        result[i] = [];
+        result[i] = '';
         for (let j = 0; j < num2.length; j++) {
             if (num1[n] == num2[j]) {
-                result[i].push(num1[n]);
+                result[i] += num1[n];
                 n++;
             }
         }
     }
-    console.log(`[${num1}]和[${num2}]的交数组为：`)
+    console.log(`[${num1}]和[${num2}]的交数组为：`);
+    let maxLengthIndex = 0;
+    result.forEach(function (value, index) {
+        if (value.length > result[maxLengthIndex])
+            maxLengthIndex = index;
+    });
+    console.log(`[${result[maxLengthIndex].split('')}]`);
     result.forEach(function (item) {
-        if (item.length > 1)
-            console.log(item);
-    })
+        if (item.length > 1 && (!result.includes(item)))
+            console.log(`[${result[maxLengthIndex].split('')}]`);
+    });
 }
 
 //test
 find([1, 2, 2, 1], [2, 2])
+find([1, 2, 2, 1], [1, 1, 2, 2])
